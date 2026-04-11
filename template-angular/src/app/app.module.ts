17663c21159e86f6005e5b02d1723590d61fda80
Isabela <guerreroisabela243@gmail.com>
2026-04-10 21:49:15 -0500

    intento

--- template-angular/src/app/app.module.ts
+++ template-angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // ← ReactiveFormsModule agregado
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
